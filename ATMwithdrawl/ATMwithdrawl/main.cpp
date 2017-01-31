111dee1f1d2f4cc14afcb47d56931d270c1a1339
BrotherLuke <824896@dpsk12.net>
2017-01-31 12:49:52 -0700

    WARMUP

--- ATMwithdrawl/ATMwithdrawl/main.cpp
+++ ATMwithdrawl/ATMwithdrawl/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
 
 	int wda;
-	cout << "How much money would you like to withdrawl?" << endl;
+	cout << "How much money would you like to withdrawl? " << endl;
 	cin >> wda ;
 	cout << "Your balance is " << 1000 - wda << endl;
 }
---
+++ verybadjoke/verybadjoke/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+int main() {
+	int reply;
+
+	cout << "What do you call a parrot" << endl;
+	cin >> reply;
+	cout << endl << endl << endl << endl << endl << endl << endl << endl;
+
+	cout << "A tropicle bird of some sort" << endl;
+}
